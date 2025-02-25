6c26b976f392f5ec0aeb114921fcc4978d3f681d
Javier Hidalgo <francisco.hidalgo.nula@gmail.com>
2025-02-25 19:57:52 +0100

    Servlets odontologist/patients

---
+++ src/main/java/com/javierhidalgodev/clinicaodontologica/servlets/SvOdontologists.java
@@ -0,0 +1,60 @@
+package com.javierhidalgodev.clinicaodontologica.servlets;
+
+import com.javierhidalgodev.clinicaodontologica.logica.Controller;
+import com.javierhidalgodev.clinicaodontologica.logica.Odontologo;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Javi
+ */
+@WebServlet(name = "SvOdontologists", urlPatterns = {"/SvOdontologists"})
+public class SvOdontologists extends HttpServlet {
+
+    Controller controller = new Controller();
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {        
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List<Odontologo> odontologistsList = controller.getAllOdontologists();
+        
+        request.getSession().setAttribute("odontologistsList", odontologistsList);
+
+        response.sendRedirect("vistaOdontologos.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String firstName = request.getParameter("firstName");
+        String surname = request.getParameter("surname");
+        String address = request.getParameter("address");
+        String phone = request.getParameter("phone");
+        String specialization = request.getParameter("specialization");
+        String dni = request.getParameter("dni");
+        String birthday = request.getParameter("birthday");
+        String workShedule = request.getParameter("workSchedule");
+        
+        controller.createOdontologist(firstName, surname, address, phone, birthday, dni, specialization, workShedule);
+        
+        response.sendRedirect("SvOdontologists");
+        
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/java/com/javierhidalgodev/clinicaodontologica/servlets/SvPatients.java
@@ -0,0 +1,74 @@
+package com.javierhidalgodev.clinicaodontologica.servlets;
+
+import com.javierhidalgodev.clinicaodontologica.logica.Controller;
+import com.javierhidalgodev.clinicaodontologica.logica.Responsable;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Javi
+ */
+@WebServlet(name = "SvPatients", urlPatterns = {"/SvPatients"})
+public class SvPatients extends HttpServlet {
+
+    Controller controller = new Controller();
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String patientFirstName = request.getParameter("patientFirstName");
+        String patientSurname = request.getParameter("patientSurname");
+        String patientAddress = request.getParameter("patientAddress");
+        String patientPhone = request.getParameter("patientPhone");
+        String patientBirthdate = request.getParameter("patientBirthdate");
+        String patientDNI = request.getParameter("patientDNI");
+        String prepaidHealth = request.getParameter("prepaidHealth");
+        String bloodType = request.getParameter("bloodType");
+
+        String guardianFirstName = request.getParameter("guardianFirstName");
+
+        if (!guardianFirstName.trim().isEmpty()) {
+            String guardianSurname = request.getParameter("guardianSurname");
+            String guardianAddress = request.getParameter("guardianAddress");
+            String guardianPhone = request.getParameter("guardianPhone");
+            String guardianBirthdate = request.getParameter("guardianBirthdate");
+            String guardianDNI = request.getParameter("guardianDNI");
+            String relationship = request.getParameter("relationship");
+
+            Responsable guardian = controller.createGuardian(guardianFirstName, guardianSurname, guardianAddress, guardianPhone, guardianBirthdate, guardianDNI, relationship);
+            
+            controller.createPatient(patientFirstName, patientSurname, patientAddress, patientPhone, patientBirthdate, patientDNI, prepaidHealth, bloodType, guardian);
+        } else {
+            controller.createPatient(patientFirstName, patientSurname, patientAddress, patientPhone, patientBirthdate, patientDNI, prepaidHealth, bloodType, null);
+        }
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
